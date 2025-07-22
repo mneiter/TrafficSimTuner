b25334144a1692024a58eb491c269095a6a591de
merukimon <mneiter@gmail.com>
2025-07-22 21:10:14 +0300

    rename file

--- master/app/main.py
+++ master/app/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse
 
 from .models import SimulationInput, SimulationResult
 from .store import InMemoryStore
-from . import endpoints
+from . import controller
 
 app = FastAPI(title="TrafficSimTuner")
 app.state.store = InMemoryStore()
@@ -20,20 +20,20 @@ app.add_middleware(
 
 @app.get("/ping")
 def ping():
-    return endpoints.ping()
+    return controller.ping()
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return endpoints.index(request)
+    return controller.index(request)
 
 @app.post("/submit_permutations")
 async def submit(input_data: SimulationInput, background_tasks: BackgroundTasks, request: Request):
-    return await endpoints.submit(input_data, background_tasks, app.state.store)
+    return await controller.submit(input_data, background_tasks, app.state.store)
 
 @app.post("/report_result")
 async def receive_result(result: SimulationResult, request: Request):
-    return await endpoints.receive_result(result, app.state.store)
+    return await controller.receive_result(result, app.state.store)
 
 @app.get("/results")
 def get_best_result(request: Request):
-    return endpoints.get_best_result(app.state.store)
+    return controller.get_best_result(app.state.store)
